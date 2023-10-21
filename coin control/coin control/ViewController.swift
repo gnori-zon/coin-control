1c06201e42a2bdaecbe18b7a7ec72a04d573066f
gnori-zon <konasenkos@gmail.com>
2023-10-21 16:10:37 +0300

    #1 #add #global - init
    create project with xcode

---
+++ coin control/coin control/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  coin control
+//
+//  Created by Степан Конашенко on 21.10.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
