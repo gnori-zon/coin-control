265dbc79182dfc1701d09048d1ea256207c988c2
gnori-zon <konasenkos@gmail.com>
2023-10-31 00:02:01 +0300

    #1 #refactor #utils - TextFieldDecimalValidator

--- coin control/coin control/utils/TextFieldDecimalValidator.swift
+++ coin control/coin control/utils/TextFieldDecimalValidator.swift
@@ -11,18 +11,40 @@ public struct TextFieldDecimalValidator {
     
     static let validate: (UITextField, NSRange, String) -> Bool = { textField, _, string in
         
-        let regex = "."
+        let point = "."
+        let regexDigit = "[1-9]"
+        let allowedCharsWithoutZero = "123456789"
         let allowedChars = "0123456789"
         let allowedCharsWithPoint = ".0123456789"
+        let allowedCharsWithPointAndWithoutZero = ".123456789"
         
         let currentAllowedChars: String
         
-        if textField.text?.contains(regex) ?? false {
-            currentAllowedChars = allowedChars
+        if textField.text?.contains(point) ?? false {
+            if textField.text?.match(regexDigit) ?? false {
+                currentAllowedChars = allowedChars
+            } else {
+                currentAllowedChars = allowedCharsWithoutZero
+            }
         } else {
-            currentAllowedChars = allowedCharsWithPoint
+            if textField.text?.match(regexDigit) ?? false {
+                currentAllowedChars = allowedCharsWithPoint
+            } else {
+                currentAllowedChars = allowedCharsWithPointAndWithoutZero
+            }
         }
         return NSCharacterSet(charactersIn: currentAllowedChars).isSuperset(of: CharacterSet(charactersIn: string))
+    }
+}
+
+fileprivate extension String {
+    
+    func match(_ pattern: String) -> Bool {
         
+        do {
+            return try self.matches(of: Regex(pattern)).count > 0
+        } catch {
+            return false
+        }
     }
 }
