f978d6b40070a2077e1cb7bba7c6ba8102ffd90f
gnori-zon <konasenkos@gmail.com>
2023-11-09 01:30:45 +0300

    #1 #add #dto - RatioCurrency

---
+++ coin control/coin control/dto/RatioCurrency.swift
@@ -0,0 +1,28 @@
+//
+//  RatioCurrency.swift
+//  coin control
+//
+//  Created by Stepan Konashenko on 08.11.2023.
+//
+
+import Foundation
+
+// MARK: - RatioCurrencyProtocol
+
+public protocol RatioCurrencyProtocol {
+    
+    var type: CurrencyType { get }
+    var value: Decimal { get }
+}
+
+// MARK: - RatioCurrency
+
+public struct RatioCurrency: RatioCurrencyProtocol {
+    
+    public let type: CurrencyType
+    public let value: Decimal
+}
+
+// MARK: - extension Codable
+
+extension RatioCurrency: Codable {}
